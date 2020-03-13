7b431e9c5595a15079685e3fff164b10b1c8ea32
shikachii <shikachii095@gmail.com>
2020-03-14 01:20:07 +0900

    [modify] リンクを追加

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -3,10 +3,13 @@
 		<div class="title">- Links -</div>
 		<div class="description">
 			<ul>
-				<li><a href="https://twitter.com">Twitter(@shikachii)</a></li>
+				<li><a href="https://twitter.com/shikachii">Twitter(@shikachii)</a></li>
 				<!--<li><span class="qr-code">■</span></li>-->
-				<li><a href="https://github.com">GitHub(@shikachii)</a></li>
+				<li><a href="https://github.com/shikachii">GitHub(@shikachii)</a></li>
+				<li><a href="https://atcoder.jp/users/shikachii">AtCoder(<span class="green">shikachii</span>)</a></li>
+				<li><a href="https://keybase.io/shikachii">Keybase(@shikachii)</a></li>
 				<li><a href="https://shikachii.hatenablog.com">Hatena Blog(id:shikachii)</a></li>
+				<li><a href="https://instagram.com/shikachii095">Instagram(@shikachii095)</a></li>
 				<li><a href="https://sp12.iidx.app/sheets/0719-9172/hard">IIDX☆12ハード参考表(0719-9172)</a></li>
 			</ul>
 		</div>
@@ -36,6 +39,17 @@ ul {
 .qr-code {
 	display: none;
 	font-size: 200px;
+}
+
+/* ankerタグ内部のgreenクラスも同時に色が変わる */
+a:hover > .green {
+	color: #55D555;
+	transition: color 200ms;
+}
+
+.green {
+	color: #008000;
+	transition: color 200ms;
 }
 
 </style>
