15ac3f7ed65a5e20f9148f481272c8afdd40b6dd
shikachii <shikachii095@gmail.com>
2020-03-14 04:47:06 +0900

    [modify] アイコン画像の表示

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,9 +1,7 @@
 <template>
 	<div id="info" class="content">
-		<div class="title">- Info -</div>
-		<div class="icon">
-			○
-		</div>
+		<div class="title">Info</div>
+		<img src="https://pbs.twimg.com/profile_images/1068206176813240320/e-XuzUj9.jpg" class="icon" alt="icon" />
 		<div class="description">
 			しかちです。
 			ラーメンとかお酒とか好きです。<br>
@@ -19,11 +17,11 @@ export default class Info extends Vue { }
 </script>
 
 <style scoped>
-#info {
-}
-
 .icon {
 	margin-top: 20px;
-	font-size: 100px;
+	width: 100px;
+	height: 100px;
+	border-radius: 50%;
+	box-shadow: 3px 3px 13px 1px #888;
 }
 </style>
