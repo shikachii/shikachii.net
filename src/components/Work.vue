e289779674f10a7eb2e4956988177f8baa4ed431
shikachii <shikachii095@gmail.com>
2020-03-28 00:22:36 +0900

    [add] Workコンポーネントの追加でitarableなコンテンツの変数化

---
+++ src/components/Work.vue
@@ -0,0 +1,47 @@
+<template>
+	<div class="work">
+		<div class="w-title"><a :href="url">{{ title }}</a></div>
+		<div class="w-description">{{ description }}</div>
+		<img v-if="imgUrl!==''"
+			:src="imgUrl" class="w-screenshot" alt="screenshot"/>
+	</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Work extends Vue {
+	@Prop({ default: ''})
+	title!: string
+
+	@Prop()
+	url?: string
+	
+	@Prop()
+	imgUrl?: string
+	
+	@Prop()
+	description?: string
+}
+</script>
+
+<style scoped>
+.w-title {
+	font-size: 24px;
+	margin-bottom: 5px;
+}
+
+.w-description {
+	font-size: 18px;
+	margin-bottom: 5px;
+}
+
+.w-screenshot {
+	max-width: 400px;
+	max-height: 400px;
+	width: 100%;
+	height: 100%;
+	margin-bottom: 5px;
+}
+</style>
--- src/components/Works.vue
+++ src/components/Works.vue
@@ -3,14 +3,13 @@
 		<div class="title">Works</div>
 		<div class="description">
 			<ul>
-				<li>
-					<div class="w-title"><a href="https://github.com/shikachii/QR-generator">QR-generator</a></div>
-					<img src="https://pbs.twimg.com/media/ESWUuRLUcAAC8Yf?format=png" class="w-screenshot" alt="screenshot"/>
-					<div class="w-description">WebページのURLをQRコードとして表示するChrome拡張</div>
-				</li>
-				<li>
-					<div class="w-title"><a href="https://shikachii.net">shikachii.net</a></div>
-					<div class="w-description">このサイトです</div>
+				<li v-for="(work, index) in works" :key=index>
+					<Work
+						:title="work.title"
+						:url="work.url"
+						:imgUrl="work.imgUrl"
+						:description="work.description"
+					/>
 				</li>
 			</ul>
 		</div>
@@ -19,9 +18,30 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import Work from '@/components/Work.vue'
 
-@Component
-export default class Works extends Vue { }
+@Component({
+	components: {
+		Work,
+	},
+})
+export default class Works extends Vue {
+	title:string = 'QR-generator'
+	works: object[] = [
+		{ 
+			title: 'QR-generator',
+			url: 'https://github.com/shikachii/QR-generator',
+			imgUrl: 'https://pbs.twimg.com/media/ESWUuRLUcAAC8Yf?format=png',
+			description: 'WebページのURLをQRコードとして表示するChrome拡張'
+		},
+		{
+			title: 'shikachii.net',
+			url: 'https://shikachii.net',
+			imgUrl: '',
+			description: 'このサイトです'
+		},
+	]	
+}
 </script>
 
 <style scoped>
