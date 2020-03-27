6510795468946f52f947fbc2cae7a8114eafbcca
shikachii <shikachii095@gmail.com>
2020-03-27 23:15:19 +0900

    [modify] 自己紹介文を変更

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -2,10 +2,13 @@
 	<div id="info" class="content">
 		<div class="title">Info</div>
 		<img src="https://pbs.twimg.com/profile_images/1068206176813240320/e-XuzUj9.jpg" class="icon" alt="icon" />
+		<!-- GitHubアイコン 本当に必要なのか
+		<img src="https://avatars1.githubusercontent.com/u/16307592" class="icon" alt="icon" />
+		-->
 		<div class="description">
-			しかちです。
-			ラーメンとかお酒とか好きです。<br>
-			プログラミングやIIDXをやっています。
+			しかちといいます。<br>
+			ラーメンを(作る/食べる)のが好きです。<br>
+			プログラミングやRe:ステージ！、BeatmaniaIIDXなども好きです。
 		</div>
 	</div>
 </template>
