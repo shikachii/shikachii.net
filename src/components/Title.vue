f5d44b78fb67821fa3d3e4cae63c399eda3b4496
shikachii <shikachii095@gmail.com>
2021-05-29 03:23:41 +0900

    :wrench: トップページの案内をタグで表示

---
+++ src/components/Title.vue
@@ -0,0 +1,40 @@
+<template>
+  <div :class="$style.root">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue';
+
+export default Vue.extend({
+});
+</script>
+
+<style module>
+.root {
+  font-size: 24px;
+}
+
+.root::before {
+  content: '';
+  display: inline-block;
+  background: #333;
+  width: 30px;
+  height: 2px;
+  margin-right: 8px;
+  margin-top: -4px;
+  vertical-align: middle;
+}
+
+.root::after {
+  content: '';
+  display: inline-block;
+  background: #333;
+  width: 30px;
+  height: 2px;
+  margin-left: 8px;
+  margin-top: -4px;
+  vertical-align: middle;
+}
+</style>
