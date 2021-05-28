a93e30f7305d46b2ce86c5438fa3a3b70b69280b
shikachii <shikachii095@gmail.com>
2021-05-29 02:19:14 +0900

    :sparkles: キャリアページを追加

---
+++ src/components/Tag.vue
@@ -0,0 +1,32 @@
+<template>
+  <div :class="$style.root">
+    <div :class="$style.tag">
+      #{{ name }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue';
+
+export default Vue.extend({
+  props: {
+    name: { type: String as PropType<string> },
+  },
+});
+</script>
+
+<style module>
+.root {
+}
+
+.tag {
+  border-radius: 30px;
+  padding: 5px 10px;
+
+  font-weight: normal;
+
+  color: #eee;
+  background: #888;
+}
+</style>
