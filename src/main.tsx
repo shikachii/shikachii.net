7de12a46179b6783dcb149e5c95a3c917a4ad25d
watanabe-mizuki1 <watanabe-mizuki1@dmm.com>
2024-01-29 00:55:30 +0900

    chore: prettier導入

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 
-import { App } from './App';
+import { App } from './App'
 
-const aaa = '';
+const aaa = ''
 
 const root = createRoot(document.getElementById('root')!)
 root.render(
   <StrictMode>
     <App />
   </StrictMode>
-);
+)
